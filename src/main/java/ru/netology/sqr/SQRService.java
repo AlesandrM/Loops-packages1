39c17acdedd04da2f726bdddf2760359deb3cc11
Aleksandr Madzharov <madzharov-aleks@mail.ru>
2022-01-05 19:11:54 +0500

    src/main/java/ru/netology/sqr/SQRService.java

---
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -0,0 +1,16 @@
+package ru.netology.sqr;
+
+public class SQRService {
+
+
+    public int square(int firstNumber, int secondNumber) {
+        int counter = 0;
+
+        for (int j = firstNumber; j < secondNumber; j++) {
+            if (j * j >= 200 && j * j <= 300) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+}
